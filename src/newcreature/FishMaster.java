1d003ffab382255879f578ccc59e2e50ed1dfcfc
BliznyukS <bliznyuk.sergey@gmail.com>
2016-01-23 14:55:58 +0200

    Создание животного и наследованая от него рыба. Переопределение.

--- src/newcreature/FishMaster.java
+++ src/newcreature/FishMaster.java
@@ -2,12 +2,13 @@ package newcreature;
 
 public class FishMaster {
     public static void main(String[] args){
+        String fishReaction;
         Fish myFish = new Fish();
         myFish.dive(2);
         myFish.dive(97);
         myFish.dive(3);
-
+        fishReaction = myFish.say("Привет!");
+        System.out.println(fishReaction);
         myFish.sleep();
-        //myFish.say("Привет!");
     }
 }
